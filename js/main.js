6846db4ae5bc72bfea22d99f87c502ff83bec5a3
Joel Maupin <jmaupin82@gmail.com>
2013-07-15 14:23:17 -0500

    Final Accel Test V1

--- js/main.js
+++ js/main.js
@@ -1,28 +1,42 @@
 
-navigator.accelerometer.getCurrentAcceleration(accelerometerSuccess, accelerometerError);
+//navigator.accelerometer.getCurrentAcceleration(accelerometerSuccess, accelerometerError);
 
-var watchID = navigator.accelerometer.watchAcceleration(Success, Error, [Options]);
+
 
 
 $(document).ready(function(){
     //this is all code taken from TuxTutor.com
 
-    //first we want to wait for PhoneGap to load
-    document.addEventListener("deviceready", loaded, false)
-    //PhoneGap is loaded
-    function loaded(){
-        navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+    //wait for PhoneGap to load
+    document.addEventListener("deviceready", loaded, false);
+    // PhoneGap is ready
+    function loaded() {
+        startWatch();
     }
-    //Get the current Acceleration data if Successful
-    function onSuccess(acceleration){
-        alert('Acceleration X: ' + acceleration.x + '\n' +
-          'Acceleration Y: ' + acceleration.y + '\n' +
-          'Acceleration Z: ' + acceleration.z + '\n' +
-          'Timestamp: '      + acceleration.timestamp + '\n');
+    // Start watching the acceleration
+    function startWatch() {
+        // Update acceleration every 3 seconds
+        var options = { frequency: 3000 };
+        watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
     }
-    // alert if there is an error
-    function onError(){
-        alert("Error");
+    // Stop watching the acceleration
+    function stopWatch() {
+        if (watchID) {
+            navigator.accelerometer.clearWatch(watchID);
+            watchID = null;
+        }
+    }
+    // Success
+    function onSuccess(acceleration) {
+        var element = document.getElementById('accelerometer');
+        element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+                            'Acceleration Y: ' + acceleration.y + '<br />' +
+                            'Acceleration Z: ' + acceleration.z + '<br />' +
+                            'Timestamp: '      + acceleration.timestamp + '<br />';
+    }
+     // Error
+    function onError() {
+        alert('onError!');
     }
 
 });
