8cff9997dc5cce73ab3676fe03e1af00d2182ff6
Joel Maupin <jmaupin82@gmail.com>
2013-07-15 11:31:42 -0500

    New Accel test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 
+navigator.accelerometer.getCurrentAcceleration(accelerometerSuccess, accelerometerError);
+
+var watchID = navigator.accelerometer.watchAcceleration(Success, Error, [Options]);
+
 
 $(document).ready(function(){
-    console.log("This is a test to see if my javascript works.");
+    //this is all code taken from TuxTutor.com
+
+    //first we want to wait for PhoneGap to load
+    document.addEventListener("deviceready", loaded, false)
+    //PhoneGap is loaded
+    function loaded(){
+        navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+    }
+    //Get the current Acceleration data if Successful
+    function onSuccess(acceleration){
+        alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+    }
+    // alert if there is an error
+    function onError(){
+        alert("Error");
+    }
+
 });
